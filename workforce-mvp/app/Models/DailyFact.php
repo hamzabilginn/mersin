f743ac892335c393b08c00fcfbab27d717632fbd
hamzabilginn <eposta_adresini_yaz@ornek.com>
2026-07-05 12:24:31 +0300

    Remove all files except yeni.pdf

--- workforce-mvp/app/Models/DailyFact.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class DailyFact extends Model {
-    protected $guarded = [];
-    public function plan() { return $this->belongsTo(DailyPlan::class, 'plan_id'); }
-}
--- workforce-mvp/app/Models/WbsItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class WbsItem extends Model {
-    protected $primaryKey = 'zzz_code';
-    public $incrementing = false;
-    protected $keyType = 'string';
-    protected $guarded = [];
-}
